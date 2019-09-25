c1fc6d6eb0241b5b4f68915fd1a3466d1aec15a8
MOUTTALIB <yassine.mouttalib@capgemini.com>
2019-09-25 10:29:55 +0100

    structure de base 1

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { BodyComponent } from './body/body.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const routes: Routes = [
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
